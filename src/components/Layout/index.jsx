91e8bc5486b54e9317cee1def6e7f28e12c4e22e
EAE00-src <edwin.edouard00@gmail.com>
2026-02-26 16:49:40 -0500

    V0.5.2 -Tweaks to Loader component logic and how it resides within the layout

--- src/components/Layout/index.jsx
+++ src/components/Layout/index.jsx
@@ -14,37 +14,25 @@ const Layout = () =>{
     const location = useLocation();
 
     const [loading, setLoading] = useState(true);
-    const [visible, setVisible] = useState(true);
     //Mounting and unmounting of loader
     useEffect(() =>{
         setLoading(true)
-        setVisible(true)
         const timer = setTimeout(() => {
             setLoading(false)
         }, 1000)
         //Every time the page (location) changes,  run the Loader for x second(s)
         return () => clearTimeout(timer)//cleanup
     }, [location]);
-    //Fadeout timer of loader
-    useEffect(() => {
-        if(!loading) {
-            const fadeTimer = setTimeout(() =>{
-                setVisible(false)
-            }, 800) //just before loader unmounts
-            return () => clearTimeout(fadeTimer)//cleanup timeout token
-        }
-        //only fires when loading state changes
-    }, [loading]);
 
     return (
         <div className='app'>
-            {/*If loading is true, display the loader until false state returns.
-            Loading controls fade direction, visible controls mount/unmount */}
-            {visible && <Loader fading={!loading} />}
+            
             <Sidebar />
             <div className='page'>
                 <DecoTags tag={`body`} tagType='tags top-tags' />
-
+                {/*If loading is true, display the loader until false state returns.
+            loading controls fade direction, visible controls mount/unmount*/}
+            {loading && <Loader />}
                 <Outlet />
 
                 <span className="tags bottom-tags">
--- src/components/Loader/Loader.jsx
+++ src/components/Loader/Loader.jsx
@@ -1,10 +1,10 @@
 import './loader.scss'
 import { HashLoader } from "react-spinners";
 
-const Loader = ({fading}) => {
+const Loader = () => {
     return (
-        <div className={`loader-overlay ${fading ? 'fade-out' : 'fade-in'}`}>
-            <HashLoader size={70} color="#00D8FF" />
+        <div className='loader-overlay'>
+            <HashLoader size={75} color="#00D8FF" />
         </div>
     )
 }
