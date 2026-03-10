173039ead6da75513135a0ebfb767571fe77ca6e
EAE00-src <edwin.edouard00@gmail.com>
2026-03-10 02:41:31 -0400

    V1.1.4 -Vite sitemap plugin initialized

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import sitemapPlugin from 'vite-plugin-sitemap'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    sitemapPlugin({
+      hostname: 'https://EAE00-src.github.io/EE-Portfolio_Website/',
+      dynamicRoutes: ['/', '/about', '/portfolio', '/contact']
+    })
+  ],
 })
