c934d3e5542b1e94d00ac2b5e2af8ea6aab5a932
EAE00-src <edwin.edouard00@gmail.com>
2026-03-10 03:32:02 -0400

    V1.2.1 -BrowserRouter basename updated

--- src/main.jsx
+++ src/main.jsx
@@ -8,7 +8,7 @@ import { HelmetProvider } from 'react-helmet-async'
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <HelmetProvider>
-      <BrowserRouter>
+      <BrowserRouter basename='/EE-Portfolio_Website/'>
         <App />
       </BrowserRouter>
     </HelmetProvider>
